4eb177760a72575f10f6ce057375031f9d7eb57d
NicolasSocquetJuglard <socquetjuglardnicolas@gmail.com>
2023-12-10 00:50:45 +0100

    Ajout boutons "Retour" et "Confirmer" au questionnaire refus et amélioration de sa mise en page.

--- Maquette/app/src/main/java/com/csm2s/maquette/AnswerNoActivity.kt
+++ Maquette/app/src/main/java/com/csm2s/maquette/AnswerNoActivity.kt
@@ -1,11 +1,21 @@
 package com.csm2s.maquette
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AnswerNoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer_no)
+
+        val buttonBack = findViewById<Button>(R.id.buttonFromQuestionnaireToHome)
+        buttonBack.setOnClickListener {
+            Intent(this, MainActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+
     }
 }
