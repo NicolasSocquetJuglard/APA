d2b8aa10d68aad087a3cb0e96608dc9e0f8f87a2
DamienVarlet <91741762+DamienVarlet@users.noreply.github.com>
2024-02-08 13:07:49 +0100

    message retour accueil

--- Maquette/app/src/main/java/com/csm2s/maquette/QuestionnairePostApaActivity.kt
+++ Maquette/app/src/main/java/com/csm2s/maquette/QuestionnairePostApaActivity.kt
@@ -1,11 +1,29 @@
 package com.csm2s.maquette
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class QuestionnairePostApaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionnaire_post_apa)
+
+        val buttonConfirmQuestionnairePostApa =
+            findViewById<Button>(R.id.buttonConfirmQuestionnairePostApa)
+        buttonConfirmQuestionnairePostApa.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setTitle("Merci !")
+            dialogBuilder.setMessage("Vos réponses ont bien été envoyées")
+            dialogBuilder.setPositiveButton("Retour à l'accueil") { alertDialog, which ->
+                Intent(this, MainActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+            val alertDialog = dialogBuilder.create()
+            alertDialog.show()
+        }
     }
 }
